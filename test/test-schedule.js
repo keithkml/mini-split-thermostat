2856828a02f5f4c707c9811e29e81571c67c77a0
Keith Lea <keithl@gmail.com>
2019-11-10 08:56:13 -0500

    Add test for scheduling rules without all props

--- test/test-schedule.js
+++ test/test-schedule.js
@@ -8,6 +8,8 @@ const midnight = Date.parse("2019-11-10 00:00")
 const one_am = Date.parse("2019-11-10 01:00")
 const two_am = Date.parse("2019-11-10 02:00")
 
+const right_before_noon = Date.parse("2019-11-10 11:59:58")
+const noon = Date.parse("2019-11-10 12:00")
 const one_pm = Date.parse("2019-11-10 13:00")
 const two_pm = Date.parse("2019-11-10 14:00")
 
@@ -39,6 +41,36 @@ test("basic schedule", async function(t) {
   clock.setSystemTime(one_am)
   clock.tick(1000)
   t.equal(o.part, "night")
+
+  clock.uninstall()
+  t.end()
+})
+
+test("allows properties to appear only in some rules", async function(t) {
+  const clock = lolex.install({ now: two_am })
+
+  const o = {}
+
+  let s = new Schedule(o, {
+    "6am": { part: "day" },
+    "6pm": { part: "night" },
+    "12pm": { lunch: "yes" },
+    "1pm": { lunch: "no" }
+  })
+
+  t.deepEqual(o, { part: "night", lunch: "no" })
+
+  clock.setSystemTime(right_before_noon)
+  clock.tick(1000)
+  t.deepEqual(o, { part: "day", lunch: "no" })
+
+  clock.setSystemTime(noon)
+  clock.tick(1000)
+  t.deepEqual(o, { part: "day", lunch: "yes" })
+
+  clock.setSystemTime(one_pm)
+  clock.tick(1000)
+  t.deepEqual(o, { part: "day", lunch: "no" })
 
   clock.uninstall()
   t.end()
